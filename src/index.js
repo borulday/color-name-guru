6d87fc387c1a8af9ee00c212b27ceec8dfbf7bb8
borulday <m.gokay@borulday.com>
2019-05-20 10:44:26 +0300

    Migrate shared styleguide

--- src/index.js
+++ src/index.js
@@ -10,23 +10,32 @@
   */
 
 const SPACE = 2;
-const messageKingdom = [
-    "🕺 Holly dance!", 
-    "💃 Holly dance!", 
-    "🤩 What a name!", 
-    "🎸 You rock!", 
-    "🤘 You rock!", 
-    "🎳 Strike!", 
-    "🙌 Perfect name!", 
-    "🥇 Golden medal!", 
-    "🎯 Hit the Bull's eye",
-    "👆 That works!",
-    "🧠 Brilliant choice!"
-];
 
-import namedColors from 'color-name-list';
-import nearestColor from 'nearest-color';
-import changeCase from 'change-case';
+import { OPTION_NAMES } from "./constants";
+import { alternateColor, getResourceContainer, getResources } from "./utils";
+
+function colors(context) {
+    var useLinkedStyleguides = context.getOption(OPTION_NAMES.USE_LINKED_STYLEGUIDES);
+    var { container, type } = getResourceContainer(context);
+    var allColors = getResources(container, type, useLinkedStyleguides, "colors");
+
+    const output = JSON.stringify({
+        source: type,
+        content: allColors.map(function(color) {
+            return alternateColor(container.type, color);
+        })
+    }, null, SPACE);
+    
+    return {
+        code: output,
+        language: "json"
+    };
+}
+
+/**
+ * 
+ * Deprecated Methods
+ */
 
 function styleguideColors(context, colors) {
     const alternateColors = colors.map(function(color) {
@@ -41,57 +50,11 @@ function styleguideColors(context, colors) {
 
 function comment(context, text) {
     return '// Color Name Suggestions\n' +
-           '// No need to change if name suggestion starts with an emoji';
-}
-
-/**
- * Suggest an alternate color
- * @param {String} projectType project type on Zeplin
- * @param {Object} color current color information
- * @returns {Object} 
- */
-function alternateColor(projectType, color) {
-    const colors = {};
-
-    namedColors.forEach(namedColor => {
-        colors[namedColor.name] = namedColor.hex;
-    });
-
-    const nearestColorInformation = nearestColor.from(colors);
-    
-    const alternateColor = nearestColorInformation({ r: color.r, g: color.g, b: color.b });
-    const alternateColorName = alternateColor.name + (color.a == 1 ? "" : parseInt(color.a * 100));
-
-    let suggestion = "";
-    switch (projectType) {
-        case "ios":
-        case "osx":
-            suggestion = changeCase.camelCase(alternateColorName);
-            break;
-
-        case "web":
-            suggestion = changeCase.paramCase(alternateColorName);
-            break;
-
-        case "android":
-            suggestion = changeCase.snakeCase(alternateColorName);
-            break;
-
-        default:
-            suggestion = "";
-    }
-
-    if (suggestion.toLowerCase() == color.name.toLowerCase()) { 
-        suggestion = messageKingdom[Math.floor(Math.random() * messageKingdom.length)];
-    }
-
-    return {
-        current: color.name,
-        suggestion: suggestion
-    };
+           '// No need to change if the name suggestion starts with an emoji';
 }
 
 export default {
+    colors,
     styleguideColors,
     comment
 };
